7fb02455647e3b790ebf3610387d6f4061efa269
r.shah <r.shah@axelor.com>
2020-04-29 15:09:45 +0530

    Add new assets

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import SaveIcon from "./save.png";
+import ImageIcon from "./image.png";
+import UploadIcon from "./upload.png";
+import DownloadIcon from "./download.png";
+
+export { SaveIcon, ImageIcon, UploadIcon, DownloadIcon };
