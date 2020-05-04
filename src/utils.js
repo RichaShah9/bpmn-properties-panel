a540c5d0844ff159a65f99bead48dd02bfe91a0e
r.shah <r.shah@axelor.com>
2020-05-04 11:16:38 +0530

    Update download type

--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,7 @@ const download = (entity, name) => {
   let encodedData = encodeURIComponent(entity);
   let dl = document.createElement("a");
   document.body.appendChild(dl);
-  dl.setAttribute(
-    "href",
-    "data:application/bpmn20-xml;charset=UTF-8," + encodedData
-  );
+  dl.setAttribute("href", "data:image/svg+xml;utf-8," + encodedData);
   dl.setAttribute("download", name);
   dl.click();
 };
