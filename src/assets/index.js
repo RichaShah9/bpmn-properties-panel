da5e16417576ad759173f453d652f18c3b1a69fd
r.shah <r.shah@axelor.com>
2020-05-22 17:24:24 +0530

    Add icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,16 @@ import SaveIcon from "./save.png";
 import ImageIcon from "./image.png";
 import UploadIcon from "./upload.png";
 import DownloadIcon from "./download.png";
+import ZoomInIcon from "./zoomIn.png";
+import ZoomOutIcon from "./zoomOut.png";
+import ResetIcon from "./reset.png";
 
-export { SaveIcon, ImageIcon, UploadIcon, DownloadIcon };
+export {
+  SaveIcon,
+  ImageIcon,
+  UploadIcon,
+  DownloadIcon,
+  ZoomInIcon,
+  ZoomOutIcon,
+  ResetIcon,
+};
