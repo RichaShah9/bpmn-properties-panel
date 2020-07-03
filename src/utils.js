4f439abd535b8758e5e9a4eeb9e6815a21281154
r.shah <r.shah@axelor.com>
2020-07-03 15:27:25 +0530

    Add translation function

--- src/utils.js
+++ src/utils.js
@@ -7,4 +7,11 @@ const download = (entity, name) => {
   dl.click();
 };
 
-export { download };
+function translate(str) {
+  if (window._t && typeof str === "string") {
+    return window._t(str);
+  }
+  return str;
+}
+
+export { download, translate };
