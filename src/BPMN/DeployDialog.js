5b6b7fa8cf71ac052e5f30a4a8705771b0cf5f37
r.shah <r.shah@axelor.com>
2020-07-13 16:53:57 +0530

    Add deploy dialog

---
+++ src/BPMN/DeployDialog.js
@@ -0,0 +1,154 @@
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Button,
+  Table,
+  TableContainer,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableBody,
+  Paper,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import _ from "lodash";
+
+import Select from "./components/Select";
+
+const useStyles = makeStyles({
+  dialogPaper: {
+    padding: 5,
+    minWidth: 300,
+    overflow: "auto",
+  },
+  button: {
+    textTransform: "none",
+    margin: "10px 0px",
+  },
+  select: {
+    minWidth: 250,
+    marginLeft: 10,
+  },
+  tableCell: {
+    padding: "0px 10px",
+    textAlign: "left",
+  },
+  tableHead: {
+    fontWeight: 600,
+    fontSize: 12,
+  },
+});
+
+export default function DeployDialog({ open, onClose, ids, onOk }) {
+  const { oldIds, currentIds } = ids || {};
+  const [oldSelectedElements, setOldElements] = useState(null);
+  const [wkfMigrationMap, setWkfMigrationMap] = useState([]);
+
+  const classes = useStyles();
+
+  const handleAdd = (oldEle, newEle) => {
+    const newElement = currentIds.find((ele) => ele.name === newEle.name);
+    setWkfMigrationMap([
+      ...wkfMigrationMap,
+      { oldNode: oldEle, currentNode: newElement },
+    ]);
+    setOldElements([...(oldSelectedElements || []), { ...oldEle }]);
+  };
+
+  const onConfirm = () => {
+    let cloneWkfMigrationMap = [...(wkfMigrationMap || [])];
+    let excludeElements = _(oldIds)
+      .differenceBy(oldSelectedElements, "id", "name")
+      .map(_.partial(_.pick, _, "id", "name"))
+      .value();
+
+    excludeElements &&
+      excludeElements.forEach((ele) => {
+        cloneWkfMigrationMap = [
+          ...cloneWkfMigrationMap,
+          {
+            oldNode: ele,
+            currentNode: null,
+          },
+        ];
+      });
+
+    onOk(cloneWkfMigrationMap);
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      classes={{
+        paper: classes.dialogPaper,
+      }}
+    >
+      <DialogContent>
+        <TableContainer component={Paper}>
+          <Table size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell className={classes.tableHead} align="center">
+                  Old Node
+                </TableCell>
+                <TableCell className={classes.tableHead} align="center">
+                  Current Node
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {oldIds.map((oldEle, index) => (
+                <TableRow key={index}>
+                  <TableCell
+                    component="th"
+                    scope="row"
+                    align="center"
+                    className={classes.tableCell}
+                  >
+                    {oldEle.name}
+                  </TableCell>
+                  <TableCell
+                    component="th"
+                    scope="row"
+                    align="center"
+                    className={classes.tableCell}
+                  >
+                    <Select
+                      className={classes.select}
+                      isLabel={false}
+                      options={currentIds}
+                      update={(value) => handleAdd(oldEle, value)}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={onConfirm}
+          className={classes.button}
+          color="primary"
+          variant="outlined"
+        >
+          Ok
+        </Button>
+        <Button
+          onClick={onClose}
+          variant="outlined"
+          color="primary"
+          className={classes.button}
+        >
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
