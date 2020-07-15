9024437d9d22657eae4dbac04a0c37068a401d97
r.shah <r.shah@axelor.com>
2020-07-15 19:56:18 +0530

    Add views file

---
+++ src/BPMN/views/index.js
@@ -0,0 +1,6 @@
+import DeployDialog from "./DeployDialog";
+import DialogView from "./DialogView";
+import ProcessConfigDialog from "./ProcessConfigDialog";
+import ConfigRecordsDialog from "./ConfigRecordsDialog";
+
+export { DeployDialog, DialogView, ProcessConfigDialog, ConfigRecordsDialog };
