5f2239b15bf79509fe0f9790efe347bcb6a00b38
r.shah <r.shah@axelor.com>
2020-07-20 20:43:58 +0530

    Move select records dialog

--- src/BPMN/Modeler/views/index.js
+++ src/BPMN/Modeler/views/index.js
@@ -1,6 +1,6 @@
 import DeployDialog from "./DeployDialog";
 import DialogView from "./DialogView";
 import SelectRecordsDialog from "./SelectRecordsDialog";
-import MigrateRecordsDialog from "./migrate-records/MigrateRecordsDialog"
+import MigrateRecordsDialog from "./migrate-records/MigrateRecordsDialog";
 
 export { DeployDialog, DialogView, SelectRecordsDialog, MigrateRecordsDialog };
