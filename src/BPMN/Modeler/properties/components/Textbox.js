ae6e84e45ff90bc8e91a4f71017644e07b72a2de
r.shah <r.shah@axelor.com>
2020-07-20 20:43:38 +0530

    Add textbox component

---
+++ src/BPMN/Modeler/properties/components/Textbox.js
@@ -0,0 +1,49 @@
+import React from "react";
+import classnames from "classnames";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    marginTop: 20,
+  },
+  input: {
+    "-moz-appearance": "textfield",
+    "-webkit-appearance": "textfield",
+    backgroundColor: "white",
+    border: "1px solid darkgray",
+    boxShadow: "1px 1px 1px 0 lightgray inset",
+    marginTop: 5,
+    padding: "2px 3px",
+    minHeight: 16,
+    display: "flex",
+  },
+  resizable: {
+    overflowY: "auto",
+    resize: "vertical",
+  },
+  label: {
+    fontWeight: "bolder",
+    display: "inline-block",
+    verticalAlign: "middle",
+    color: "#666",
+    marginBottom: 3,
+  },
+});
+
+export default function Textbox({ label, value, isResizable = false }) {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <label className={classes.label}>{label}</label>
+      <div
+        contentEditable={true}
+        suppressContentEditableWarning={true}
+        className={classnames(classes.input, isResizable && classes.resizable)}
+      >
+        {value}
+      </div>
+    </div>
+  );
+}
