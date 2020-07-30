6694a332ae5375c215b582cd05aadb1a11881c79
r.shah <r.shah@axelor.com>
2020-07-30 17:48:46 +0530

    Update table component

--- src/BPMN/Modeler/properties/components/Table.js
+++ src/BPMN/Modeler/properties/components/Table.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/styles";
-
+import _ from "lodash";
 const useStyles = makeStyles({
   root: {
     display: "flex",
@@ -36,8 +36,43 @@ const useStyles = makeStyles({
 //TODO - add entry
 export default function Table({ entry }) {
   const classes = useStyles();
-  const { addLabel, labels, id } = entry || {};
-  
+  const {
+    id,
+    addLabel,
+    labels,
+    addElement,
+    showDefaultEntry,
+    modelProperties,
+    getElements,
+    removeElement,
+    updateElement,
+    validate,
+  } = entry || {};
+  const [options, setOptions] = useState(null);
+  const [errors, setErrors] = useState({});
+
+  const getOptions = React.useCallback(() => {
+    const options = getElements && getElements();
+    if (id === "form-field-enum-values" || id === "form-field-properties") {
+      setOptions(_.uniqBy([...(options || [])], "id"));
+    } else {
+      setOptions([...(options || [])]);
+    }
+  }, [getElements, id]);
+
+  const addNewElement = () => {
+    let element = addElement();
+    if (id === "form-field-enum-values" || id === "form-field-properties") {
+      setOptions(_.uniqBy([...(options || []), element], "id"));
+    } else {
+      setOptions([...(options || []), element]);
+    }
+  };
+
+  useEffect(() => {
+    getOptions();
+  }, [getOptions]);
+
   return (
     <div className={classes.root}>
       <div style={{ display: "flex", alignItems: "center" }}>
@@ -45,41 +80,71 @@ export default function Table({ entry }) {
         <button
           className={classes.add}
           id={`cam-extensionElements-create-${id}`}
-        //   onClick={() => addElement(element)}
+          onClick={addNewElement}
         >
           <span>+</span>
         </button>
       </div>
-      <table>
-        <tr
-          style={{
-            display: "flex",
-            width: "100%",
-            justifyContent: "space-between",
-          }}
-        >
-          {labels.map((label) => (
-            <th key={label}>{label}</th>
-          ))}
-          <th key="add"></th>
-        </tr>
-        <tr>
-          {labels.map((label) => (
-            <td key={`${label}_data`}>
-              <input type="text" style={{ width: "100%" }} />
-            </td>
-          ))}
-          <td>
-            <button
-              className={classes.clear}
-              id={`cam-extensionElements-remove-${id}`}
-              onClick={() => {}}
+      {(showDefaultEntry || (options && options.length > 0)) && (
+        <table>
+          <tbody>
+            <tr
+              style={{
+                display: "flex",
+                width: "100%",
+                justifyContent: "space-between",
+              }}
             >
-              <span style={{ fontSize: 10 }}>X</span>
-            </button>
-          </td>
-        </tr>
-      </table>
+              {labels.map((label) => (
+                <th key={label}>{label}</th>
+              ))}
+              <th key="add"></th>
+            </tr>
+            {options &&
+              options.map((option, optionIndex) => (
+                <tr key={`${option}_${optionIndex}`}>
+                  <React.Fragment>
+                    {modelProperties &&
+                      modelProperties.map((label, index) => (
+                        <td key={`${option[label]}_${index}`}>
+                          <input
+                            type="text"
+                            defaultValue={option[label] || ""}
+                            style={{ width: "100%" }}
+                            onBlur={(e) => {
+                              let isValid =
+                                validate &&
+                                validate(e.target.value, optionIndex);
+                              if (isValid) {
+                                updateElement(
+                                  e.target.value,
+                                  label,
+                                  option,
+                                  optionIndex
+                                );
+                              }
+                            }}
+                          />
+                        </td>
+                      ))}
+                    <td>
+                      <button
+                        className={classes.clear}
+                        id={`cam-extensionElements-remove-${id}`}
+                        onClick={() => {
+                          removeElement(option, optionIndex);
+                          getOptions();
+                        }}
+                      >
+                        <span style={{ fontSize: 10 }}>X</span>
+                      </button>
+                    </td>
+                  </React.Fragment>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
