25edc7bcdbbf26bd4a03adc465bda665edc8752d
r.shah <r.shah@axelor.com>
2020-07-31 19:46:40 +0530

    Add custom ErrorProps

---
+++ src/BPMN/Modeler/properties/parts/CustomImplementation/ErrorProps.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  groupLabel: {
+    fontWeight: "bolder",
+    display: "inline-block",
+    verticalAlign: "middle",
+    color: "#666",
+    fontSize: "120%",
+    margin: "10px 0px",
+    transition: "margin 0.218s linear",
+    fontStyle: "italic",
+  },
+  groupContainer: {
+    marginTop: 10,
+  },
+  divider: {
+    marginTop: 15,
+    border: "1px dotted #ccc",
+  },
+});
+
+export default function ErrorProps({ element, index, label }) {
+  const [isVisible, setVisible] = useState(false);
+  const classes = useStyles();
+
+  return (
+    isVisible && (
+      <div>
+        <React.Fragment>
+          {index > 0 && <div className={classes.divider} />}
+        </React.Fragment>
+        <div className={classes.groupLabel}>{label}</div>
+        <div>ErrorProps</div>
+      </div>
+    )
+  );
+}
