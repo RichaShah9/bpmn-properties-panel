aed8216061f807e871fade6051aeee877e5eec3e
r.shah <r.shah@axelor.com>
2020-07-31 19:47:03 +0530

    Add LinkProps

---
+++ src/BPMN/Modeler/properties/parts/CustomImplementation/index.js
@@ -0,0 +1,19 @@
+import CallActivityProps from "./CallActivityProps";
+import ConditionalProps from "./ConditionalProps";
+import ErrorProps from "./ErrorProps";
+import EventProps from "./EventProps";
+import LinkProps from "./LinkProps";
+import ScriptProps from "./ScriptProps";
+import ServiceTaskDelegateProps from "./ServiceTaskDelegateProps";
+import StartEventInitiator from "./StartEventInitiator";
+
+export {
+  CallActivityProps,
+  ConditionalProps,
+  ErrorProps,
+  EventProps,
+  LinkProps,
+  ScriptProps,
+  ServiceTaskDelegateProps,
+  StartEventInitiator,
+};
