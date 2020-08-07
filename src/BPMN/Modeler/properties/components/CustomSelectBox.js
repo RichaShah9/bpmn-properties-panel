f0c6d695efa2b217b7cc8ee3dc6064438634e7bb
r.shah <r.shah@axelor.com>
2020-08-07 16:21:51 +0530

    Update custom selectbox component

--- src/BPMN/Modeler/properties/components/CustomSelectBox.js
+++ src/BPMN/Modeler/properties/components/CustomSelectBox.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/styles";
 import utils from "bpmn-js-properties-panel/lib/Utils";
+import find from "lodash/find";
 
 const useStyles = makeStyles({
   root: {
@@ -47,18 +48,110 @@ const useStyles = makeStyles({
   },
 });
 
-export default function CustomSelectBox({ entry, element }) {
-  const classes = useStyles();
-  const { label, canBeHidden, id, selectElement, newElementIdPrefix } =
-    entry || {};
+function findElementById(eventDefinition, type, id) {
+  var elements = utils.findRootElementsByType(eventDefinition, type);
+  return find(elements, function (element) {
+    return element.id === id;
+  });
+}
 
-  const [options, setOptions] = useState(null);
+export default function CustomSelectBox({
+  entry,
+  element,
+  bpmnFactory,
+  bpmnModdle,
+  definition,
+  bpmnModeler,
+  defaultOptions = [],
+}) {
+  const classes = useStyles();
+  const {
+    label,
+    canBeHidden,
+    id,
+    referenceProperty,
+    set,
+    emptyParameter = true,
+    elementType,
+    newElementIdPrefix,
+    get,
+  } = entry || {};
+  const [selectedOption, setSelectedOption] = useState("");
+  const [options, setOptions] = useState([
+    {
+      name: "",
+      value: "",
+    },
+  ]);
+
+  const setSelectedElement = React.useCallback(
+    (value) => {
+      setSelectedOption(value);
+      set(value);
+    },
+    [set]
+  );
 
   const addElement = () => {
     let prefix = newElementIdPrefix || "elem_";
     let id = utils.nextId(prefix);
-    setOptions([...(options || []), id]);
+    let name = utils.nextId(prefix);
+    let props = {};
+    if (!name) {
+      props[referenceProperty] = undefined;
+    }
+    let selectedElement = findElementById(definition, elementType, name);
+    if (!selectedElement) {
+      let root = utils.getRoot(definition);
+      selectedElement = bpmnModdle.create(elementType, {
+        id,
+        name,
+      });
+      let rootElement =
+        bpmnModeler &&
+        bpmnModeler.get("canvas").getRootElement().businessObject.$parent
+          .rootElements;
+      rootElement.push(selectedElement);
+      selectedElement.$parent = root;
+      definition[referenceProperty] = selectedElement;
+    }
+
+    setOptions([
+      ...(options || []),
+      {
+        name: `${name} (id=${id})`,
+        value: name,
+      },
+    ]);
+    setSelectedElement(name);
   };
+
+  useEffect(() => {
+    let selectedOption = get();
+    setSelectedOption(selectedOption[referenceProperty]);
+  }, [get, referenceProperty]);
+
+  // useEffect(() => {
+  //   const rootElements =
+  //     bpmnModeler &&
+  //     bpmnModeler.get("canvas").getRootElement().businessObject.$parent
+  //       .rootElements;
+  //   const elements =
+  //     rootElements && rootElements.filter((r) => r.$type === elementType);
+  //   const options =
+  //     elements &&
+  //     elements.map((element) => {
+  //       return {
+  //         value: element.name,
+  //         name: `${element.name} (id=${element.id})`,
+  //       };
+  //     });
+  //   setOptions(options || []);
+  // }, [bpmnModeler, elementType]);
+
+  useEffect(() => {
+    setOptions(defaultOptions);
+  }, [defaultOptions]);
 
   return (
     <div className={classes.root}>
@@ -75,13 +168,19 @@ export default function CustomSelectBox({ entry, element }) {
             className={classes.extensionElements}
             name="selectedExtensionElement"
             data-list-entry-container
-            onChange={() => {
-              selectElement && selectElement(element);
+            value={selectedOption}
+            onChange={(e) => {
+              setSelectedElement(e.target.value);
             }}
           >
             {options &&
               options.length > 0 &&
-              options.map((option) => <option key={option}>{option}</option>)}
+              options.map((option) => (
+                <option value={option.value} key={option.value}>
+                  {option.name}
+                </option>
+              ))}
+            {emptyParameter && <option value=""></option>}
           </select>
           <button
             className={classes.add}
