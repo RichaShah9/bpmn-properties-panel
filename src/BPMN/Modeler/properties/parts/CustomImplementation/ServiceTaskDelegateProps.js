b2e641f62694d74a55f8559dfeb8f49a4f74ac22
r.shah <r.shah@axelor.com>
2020-08-13 20:06:02 +0530

    Add DMN option for Business rule

--- src/BPMN/Modeler/properties/parts/CustomImplementation/ServiceTaskDelegateProps.js
+++ src/BPMN/Modeler/properties/parts/CustomImplementation/ServiceTaskDelegateProps.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ImplementationTypeHelper from "bpmn-js-properties-panel/lib/helper/ImplementationTypeHelper";
 import { makeStyles } from "@material-ui/core/styles";
+import { is } from "bpmn-js/lib/util/ModelUtil";
 
 import { SelectBox, TextField } from "../../components";
 import { translate } from "../../../../../utils";
@@ -12,6 +13,44 @@ function isServiceTaskLike(element) {
 function getBusinessObject(element) {
   return ImplementationTypeHelper.getServiceTaskLikeBusinessObject(element);
 }
+
+const bindingOptions = [
+  {
+    name: translate("latest"),
+    value: "latest",
+  },
+  {
+    name: translate("deployment"),
+    value: "deployment",
+  },
+  {
+    name: translate("version"),
+    value: "version",
+  },
+  {
+    name: translate("versionTag"),
+    value: "versionTag",
+  },
+];
+
+const mapDecisionResultOptions = [
+  {
+    name: "singleEntry (TypedValue)",
+    value: "singleEntry",
+  },
+  {
+    name: "singleResult (Map<String, Object>)",
+    value: "singleResult",
+  },
+  {
+    name: "collectEntries (List<Object>)",
+    value: "collectEntries",
+  },
+  {
+    name: "resultList (List<Map<String, Object>>)",
+    value: "resultList",
+  },
+];
 
 const useStyles = makeStyles({
   groupLabel: {
@@ -33,10 +72,33 @@ const useStyles = makeStyles({
   },
 });
 
+const implementationOptions = [
+  { name: translate("Java Class"), value: "class" },
+  { name: translate("Expression"), value: "expression" },
+  {
+    name: translate("Delegate Expression"),
+    value: "delegateExpression",
+  },
+  { name: translate("DMN"), value: "dmn" },
+  { name: translate("External"), value: "external" },
+];
+
 export default function ServiceTaskDelegateProps({ element, index, label }) {
   const [isVisible, setVisible] = useState(false);
   const [implementationType, setImplementationType] = useState("");
+  const [bindingType, setBindingType] = useState("latest");
+  const [resultVariable, setResultVariable] = useState(null);
   const classes = useStyles();
+
+  const getPropertyValue = (propertyName) => {
+    const bo = getBusinessObject(element);
+    return bo[propertyName];
+  };
+
+  const setPropertyValue = (propertyName, value) => {
+    const bo = getBusinessObject(element);
+    bo[propertyName] = value;
+  };
 
   useEffect(() => {
     let bo = getBusinessObject(element);
@@ -49,6 +111,10 @@ export default function ServiceTaskDelegateProps({ element, index, label }) {
       type = "delegateExpression";
     } else if (bo.topic || bo.topic === "") {
       type = "external";
+    } else if (bo.decisionRef) {
+      type = "dmn";
+      setResultVariable(bo.resultVariable);
+      setBindingType(bo.decisionRefBinding);
     } else {
       type = "";
     }
@@ -74,15 +140,17 @@ export default function ServiceTaskDelegateProps({ element, index, label }) {
             id: "implementationType",
             label: "Implementation",
             modelProperty: "implementationType",
-            selectOptions: [
-              { name: translate("Java Class"), value: "class" },
-              { name: translate("Expression"), value: "expression" },
-              {
-                name: translate("Delegate Expression"),
-                value: "delegateExpression",
-              },
-              { name: translate("External"), value: "external" },
-            ],
+            selectOptions: function () {
+              let options;
+              if (!is(element, "bpmn:BusinessRuleTask")) {
+                options = implementationOptions.filter(function (option) {
+                  return option.value !== "dmn";
+                });
+              } else {
+                options = implementationOptions;
+              }
+              return options;
+            },
             emptyParameter: true,
             get: function () {
               return { implementationType: implementationType };
@@ -96,6 +164,7 @@ export default function ServiceTaskDelegateProps({ element, index, label }) {
                 element.businessObject.resultVariable = undefined;
                 element.businessObject.topic = undefined;
                 element.businessObject.taskPriority = undefined;
+                element.businessObject.decisionRef = undefined;
               } else {
                 values.implementationType !== "external"
                   ? (element.businessObject[values.implementationType] = "")
@@ -128,6 +197,7 @@ export default function ServiceTaskDelegateProps({ element, index, label }) {
                   element.businessObject.resultVariable = undefined;
                   element.businessObject.delegateExpression = undefined;
                   element.businessObject.topic = undefined;
+                  element.businessObject.decisionRef = undefined;
                 }
               },
               validate: function (e, values) {
@@ -162,10 +232,14 @@ export default function ServiceTaskDelegateProps({ element, index, label }) {
                     element.businessObject.class = undefined;
                     element.businessObject.delegateExpression = undefined;
                     element.businessObject.topic = undefined;
+                    element.businessObject.decisionRef = undefined;
                   }
                 },
                 validate: function (e, values) {
-                  if (!values.expression && implementationType === "expression") {
+                  if (
+                    !values.expression &&
+                    implementationType === "expression"
+                  ) {
                     return { expression: "Must provide a value" };
                   }
                 },
@@ -217,16 +291,205 @@ export default function ServiceTaskDelegateProps({ element, index, label }) {
                   element.businessObject.expression = undefined;
                   element.businessObject.resultVariable = undefined;
                   element.businessObject.topic = undefined;
+                  element.businessObject.decisionRef = undefined;
                 }
               },
               validate: function (e, values) {
-                if (!values.delegateExpression && implementationType === "delegateExpression") {
+                if (
+                  !values.delegateExpression &&
+                  implementationType === "delegateExpression"
+                ) {
                   return { delegateExpression: "Must provide a value" };
                 }
               },
             }}
             canRemove={true}
           />
+        )}
+        {implementationType === "dmn" && (
+          <React.Fragment>
+            <TextField
+              element={element}
+              entry={{
+                id: "decisionRef",
+                label: translate("Decision Ref"),
+                modelProperty: "decisionRef",
+                get: function () {
+                  const bo = getBusinessObject(element);
+                  return { decisionRef: bo.decisionRef };
+                },
+                set: function (e, values) {
+                  element.businessObject.decisionRef = values.decisionRef;
+                  element.businessObject.class = undefined;
+                  element.businessObject.expression = undefined;
+                  element.businessObject.resultVariable = undefined;
+                  element.businessObject.delegateExpression = undefined;
+                  element.businessObject.topic = undefined;
+                },
+                validate: function (e, values) {
+                  if (!values.decisionRef) {
+                    return { decisionRef: "Must provide a value" };
+                  }
+                },
+              }}
+              canRemove={true}
+            />
+            <SelectBox
+              element={element}
+              entry={{
+                id: "decisionRefBinding",
+                label: "Binding",
+                modelProperty: "decisionRefBinding",
+                selectOptions: bindingOptions,
+                emptyParameter: true,
+                get: function () {
+                  return {
+                    decisionRefBinding: bindingType,
+                  };
+                },
+                set: function (e, values) {
+                  setBindingType(values.decisionRefBinding);
+                  setPropertyValue(
+                    "decisionRefBinding",
+                    values.decisionRefBinding
+                  );
+                },
+              }}
+            />
+            {bindingType === "version" && (
+              <TextField
+                element={element}
+                entry={{
+                  id: "decisionRefVersion",
+                  label: translate("Version"),
+                  modelProperty: "decisionRefVersion",
+                  get: function () {
+                    return {
+                      decisionRefVersion: getPropertyValue(
+                        "decisionRefVersion"
+                      ),
+                    };
+                  },
+                  set: function (e, values) {
+                    setPropertyValue(
+                      "decisionRefVersion",
+                      values.decisionRefVersion
+                    );
+                    setPropertyValue("decisionRefVersionTag", undefined);
+                  },
+                  validate: function (e, values) {
+                    if (
+                      !values.decisionRefVersion &&
+                      bindingType === "version"
+                    ) {
+                      return { decisionRefVersion: "Must provide a value" };
+                    }
+                  },
+                }}
+                canRemove={true}
+              />
+            )}
+            {bindingType === "versionTag" && (
+              <TextField
+                element={element}
+                entry={{
+                  id: "decisionRefVersionTag",
+                  label: translate("Version Tag"),
+                  modelProperty: "decisionRefVersionTag",
+                  get: function () {
+                    let bo = getBusinessObject(element);
+                    return {
+                      decisionRefVersionTag:
+                        bo.$attrs["camunda:decisionRefVersionTag"],
+                    };
+                  },
+                  set: function (e, values) {
+                    let bo = getBusinessObject(element);
+                    bo.$attrs["camunda:decisionRefVersionTag"] =
+                      values.decisionRefVersionTag;
+                    setPropertyValue("decisionRefVersion", undefined);
+                  },
+                  validate: function (e, values) {
+                    if (
+                      !values.decisionRefVersionTag &&
+                      bindingType === "versionTag"
+                    ) {
+                      return {
+                        decisionRefVersionTag: "Must provide a value",
+                      };
+                    }
+                  },
+                }}
+                canRemove={true}
+              />
+            )}
+            <TextField
+              element={element}
+              entry={{
+                id: "decisionRefTenantId",
+                label: translate("Tenant id"),
+                modelProperty: "decisionRefTenantId",
+                get: function () {
+                  return {
+                    decisionRefTenantId: getPropertyValue(
+                      "decisionRefTenantId"
+                    ),
+                  };
+                },
+                set: function (e, values) {
+                  setPropertyValue(
+                    "decisionRefTenantId",
+                    values.decisionRefTenantId
+                  );
+                },
+              }}
+              canRemove={true}
+            />
+            <TextField
+              element={element}
+              entry={{
+                id: "resultVariable",
+                label: translate("Result Variable"),
+                modelProperty: "resultVariable",
+                get: function () {
+                  let bo = getBusinessObject(element);
+                  let boResultVariable = bo.resultVariable;
+                  return { resultVariable: boResultVariable };
+                },
+                set: function (e, values) {
+                  if (element.businessObject) {
+                    setResultVariable(values.resultVariable);
+                    element.businessObject.resultVariable =
+                      values.resultVariable || undefined;
+                  }
+                },
+              }}
+              canRemove={true}
+            />
+            {resultVariable && (
+              <SelectBox
+                element={element}
+                entry={{
+                  id: "dmn-map-decision-result",
+                  label: translate("Map Decision Result"),
+                  selectOptions: mapDecisionResultOptions,
+                  modelProperty: "mapDecisionResult",
+                  get: function () {
+                    var bo = getBusinessObject(element);
+                    return {
+                      mapDecisionResult: bo.mapDecisionResult || "resultList",
+                    };
+                  },
+                  set: function (e, values) {
+                    setPropertyValue(
+                      "mapDecisionResult",
+                      values["mapDecisionResult"]
+                    );
+                  },
+                }}
+              />
+            )}
+          </React.Fragment>
         )}
         {implementationType === "external" && (
           <React.Fragment>
@@ -252,6 +515,7 @@ export default function ServiceTaskDelegateProps({ element, index, label }) {
                     element.businessObject.expression = undefined;
                     element.businessObject.resultVariable = undefined;
                     element.businessObject.delegateExpression = undefined;
+                    element.businessObject.decisionRef = undefined;
                   }
                 },
                 validate: function (e, values) {
