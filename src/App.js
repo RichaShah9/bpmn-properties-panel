557ae044d942e2ad1b9a9034c5cf5d1e140e31b9
r.shah <r.shah@axelor.com>
2020-08-18 14:38:12 +0530

    Remove cmmn

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import BpmnModelerComponent from "./BPMN/Modeler/BpmnModeler";
 import BpmnViewerComponent from "./BPMN/Viewer/BpmnViewer";
 import DMNModeler from "./DMN/DMNModeler";
-import CMMNModeler from "./CMMN/CMMNModeler";
-import CMMNViewer from "./CMMN/CMMNViewer";
 
 let isInstance = false;
 
@@ -12,9 +10,6 @@ const fetchId = () => {
   const regexBPMNTask = /[?&]taskIds=([^&#]*)/g; // ?id=1&taskIds=1,2
   const regexBPMNInstance = /[?&]instanceId=([^&#]*)/g; // ?instanceId=1
   const regexDMN = /[?&]type=dmn([^&#]*)/g; // ?type=dmn?id=1
-  const regexCMMN = /[?&]type=cmmn([^&#]*)/g; // ?type=cmmn?id=1
-  const regexCMMNTask = /[?&]type=cmmn([^&#]*)&id=([^&#]*)&taskIds=([^&#]*)/g; // ?type=cmmn?id=1&taskIds=1,2
-  const regexCMMNInstance = /[?&]type=cmmn([^&#]*)&instanceId=([^&#]*)/g; // ?type=cmmn?instanceId=1
 
   const url = window.location.href;
   let type = "bpmnModeler";
@@ -31,21 +26,8 @@ const fetchId = () => {
     type = "dmnModeler";
   }
 
-  while (regexCMMN.exec(url)) {
-    type = "cmmnModeler";
-  }
-
-  while (regexCMMNTask.exec(url)) {
-    type = "cmmnViewer";
-  }
-
   while (regexBPMNInstance.exec(url)) {
     type = "bpmnViewer";
-    isInstance = true;
-  }
-
-  while (regexCMMNInstance.exec(url)) {
-    type = "cmmnViewer";
     isInstance = true;
   }
 
@@ -68,10 +50,6 @@ function App() {
         <BpmnModelerComponent />
       ) : type === "bpmnViewer" ? (
         <BpmnViewerComponent isInstance={isInstance} />
-      ) : type === "cmmnModeler" ? (
-        <CMMNModeler />
-      ) : type === "cmmnViewer" ? (
-        <CMMNViewer isInstance={isInstance} />
       ) : (
         <React.Fragment></React.Fragment>
       )}
