2cfe39c5a721697b14cdfea8a3602b7c42b5ba72
r.shah <r.shah@axelor.com>
2020-08-18 17:48:07 +0530

    Update UserTask props with camunda extended props

--- src/BPMN/Modeler/properties/parts/CustomImplementation/UserTaskProps.js
+++ src/BPMN/Modeler/properties/parts/CustomImplementation/UserTaskProps.js
@@ -1,8 +1,4 @@
 import React, { useEffect, useState } from "react";
-import extensionElementsHelper from "bpmn-js-properties-panel/lib/helper/ExtensionElementsHelper";
-import elementHelper from "bpmn-js-properties-panel/lib/helper/ElementHelper";
-import cmdHelper from "bpmn-js-properties-panel/lib/helper/CmdHelper";
-import find from "lodash/find";
 import { makeStyles } from "@material-ui/core/styles";
 import { is, getBusinessObject } from "bpmn-js/lib/util/ModelUtil";
 
@@ -26,150 +22,22 @@ const useStyles = makeStyles({
   },
 });
 
-export default function UserTaskProps({
-  element,
-  index,
-  label,
-  bpmnFactory,
-  bpmnModeler,
-}) {
+export default function UserTaskProps({ element, index, label }) {
   const [isVisible, setVisible] = useState(false);
   const classes = useStyles();
 
   const getProperty = (name) => {
     const bo = getBusinessObject(element);
-    let camundaProperties = extensionElementsHelper.getExtensionElements(
-      bo,
-      "camunda:Properties"
-    );
-    const elements =
-      camundaProperties && camundaProperties[0] && camundaProperties[0].values;
-    const existingElement =
-      elements && elements.find((element) => element.name === name);
-    return existingElement && existingElement.value;
-  };
-
-  function createParent(element, bo) {
-    const bpmnFactory = bpmnModeler.get("bpmnFactory");
-
-    let parent = elementHelper.createElement(
-      "bpmn:ExtensionElements",
-      { values: [] },
-      bo,
-      bpmnFactory
-    );
-    let cmd = cmdHelper.updateBusinessObject(element, bo, {
-      extensionElements: parent,
-    });
-    return {
-      cmd: cmd,
-      parent: parent,
-    };
-  }
-
-  const createCamundaProperty = () => {
-    const bo = getBusinessObject(element);
-    let result = createParent(element, bo);
-    let camundaProperties = elementHelper.createElement(
-      "camunda:Properties",
-      {},
-      result && result.parent,
-      bpmnFactory
-    );
-    element.businessObject.extensionElements &&
-      element.businessObject.extensionElements.values.push(camundaProperties);
-  };
-
-  function isExtensionElements(element) {
-    return is(element, "bpmn:ExtensionElements");
-  }
-
-  function getPropertiesElementInsideExtensionElements(extensionElements) {
-    return find(
-      extensionElements.$parent.extensionElements &&
-        extensionElements.$parent.extensionElements.values,
-      function (elem) {
-        return is(elem, "camunda:Properties");
-      }
-    );
-  }
-
-  function getPropertiesElement(element) {
-    if (!isExtensionElements(element)) {
-      return element.properties;
-    } else {
-      return getPropertiesElementInsideExtensionElements(element);
-    }
-  }
-
-  const setValue = (name, value) => {
-    const bo = getBusinessObject(element);
-    let camundaProperties = extensionElementsHelper.getExtensionElements(
-      bo,
-      "camunda:Properties"
-    );
-
-    let propertyProps = {
-      name: name,
-      value: value,
-    };
-    let result = createParent(element, bo);
-    let properties = getPropertiesElement(result.parent);
-
-    if (camundaProperties && camundaProperties[0]) {
-      const elements = camundaProperties[0].values;
-      if (!elements) {
-        let property = elementHelper.createElement(
-          "camunda:Property",
-          propertyProps,
-          properties,
-          bpmnFactory
-        );
-        camundaProperties[0].values = [property];
-        property.value = value;
-      } else {
-        const existingElement =
-          elements && elements.find((element) => element.name === name);
-        if (!existingElement) {
-          let property = elementHelper.createElement(
-            "camunda:Property",
-            propertyProps,
-            properties,
-            bpmnFactory
-          );
-          property.value = value;
-          elements.push(property);
-        } else {
-          existingElement.value = value;
-        }
-      }
-    }
+    return (bo.$attrs && bo.$attrs[name]) || "";
   };
 
   const setProperty = (name, value) => {
     const bo = getBusinessObject(element);
-    let extensionElements = bo.extensionElements;
-    if (!extensionElements) {
-      extensionElements = elementHelper.createElement(
-        "bpmn:ExtensionElements",
-        { values: [] },
-        bo,
-        bpmnFactory
-      );
-      element.businessObject.extensionElements = extensionElements;
-      createCamundaProperty();
-      setValue(name, value);
+    if (!bo) return;
+    if (bo.$attrs) {
+      bo.$attrs[name] = value;
     } else {
-      let camundaProperties = extensionElementsHelper.getExtensionElements(
-        bo,
-        "camunda:Properties"
-      );
-      if (camundaProperties && camundaProperties[0]) {
-        setValue(name, value);
-      } else {
-        createCamundaProperty();
-        setValue(name, value);
-      }
+      bo.$attrs = { [name]: value };
     }
   };
 
@@ -194,11 +62,11 @@ export default function UserTaskProps({
             modelProperty: "completedIf",
             get: function () {
               return {
-                completedIf: getProperty("completedIf"),
+                completedIf: getProperty("camunda:completedIf"),
               };
             },
             set: function (e, values) {
-              setProperty("completedIf", values["completedIf"]);
+              setProperty("camunda:completedIf", values["completedIf"]);
             },
           }}
           canRemove={true}
@@ -211,11 +79,11 @@ export default function UserTaskProps({
             modelProperty: "buttons",
             get: function () {
               return {
-                buttons: getProperty("buttons"),
+                buttons: getProperty("camunda:buttons"),
               };
             },
             set: function (e, values) {
-              setProperty("buttons", values["buttons"]);
+              setProperty("camunda:buttons", values["buttons"]);
             },
           }}
           canRemove={true}
