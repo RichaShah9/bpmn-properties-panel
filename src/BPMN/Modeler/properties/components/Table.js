b8c34aa1fe2efb323ddb39e71442309813f9320c
r.shah <r.shah@axelor.com>
2020-09-01 12:42:01 +0530

    Update table component

--- src/BPMN/Modeler/properties/components/Table.js
+++ src/BPMN/Modeler/properties/components/Table.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { IconButton } from "@material-ui/core";
+import { Add, Close } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/styles";
-import _ from "lodash";
+
 const useStyles = makeStyles({
   root: {
     display: "flex",
@@ -8,13 +10,13 @@ const useStyles = makeStyles({
     marginTop: 5,
   },
   add: {
-    marginLeft: 10,
     backgroundColor: "#f8f8f8",
     border: "1px solid #ccc",
     height: 23,
     width: 24,
     overflow: "hidden",
     cursor: "pointer",
+    borderRadius: 0,
   },
   clear: {
     height: 23,
@@ -23,49 +25,79 @@ const useStyles = makeStyles({
     cursor: "pointer",
     backgroundColor: "#f8f8f8",
     border: "1px solid #ccc",
+    borderRadius: 0,
   },
   label: {
     fontWeight: "bolder",
-    display: "inline-block",
     verticalAlign: "middle",
     color: "#666",
     marginBottom: 3,
   },
+  tableHead: {
+    display: "table-row",
+    verticalAlign: "inherit",
+    borderColor: "inherit",
+    textAlign: "center",
+  },
+  input: {
+    padding: "3px 0px 3px 6px",
+    border: "1px solid #ccc",
+    minWidth: 30,
+    maxWidth: "95%",
+    width: "100%",
+    "&:focus": {
+      boxShadow: "rgba(82, 180, 21, 0.2) 0px 0px 1px 2px",
+      outline: "none",
+      borderColor: "rgb(82, 180, 21)",
+    },
+  },
 });
 
-//TODO - add entry
 export default function Table({ entry }) {
   const classes = useStyles();
   const {
     id,
     addLabel,
     labels,
-    addElement,
-    showDefaultEntry,
     modelProperties,
     getElements,
+    addElement,
     removeElement,
     updateElement,
-    validate,
   } = entry || {};
-  const [options, setOptions] = useState(null);
+  const [options, setOptions] = useState([]);
 
   const getOptions = React.useCallback(() => {
     const options = getElements && getElements();
-    if (id === "form-field-enum-values" || id === "form-field-properties") {
-      setOptions(_.uniqBy([...(options || [])], "id"));
-    } else {
-      setOptions([...(options || [])]);
-    }
-  }, [getElements, id]);
+    setOptions([...(options || [])]);
+  }, [getElements]);
 
   const addNewElement = () => {
-    let element = addElement();
-    if (id === "form-field-enum-values" || id === "form-field-properties") {
-      setOptions(_.uniqBy([...(options || []), element], "id"));
-    } else {
-      setOptions([...(options || []), element]);
-    }
+    setOptions([
+      ...(options || []),
+      {
+        name: "",
+        value: "",
+      },
+    ]);
+    addElement({
+      name: "",
+      value: "",
+    });
+  };
+
+  const remove = (optionIndex) => {
+    const cloneOptions = [...(options || [])];
+    cloneOptions.splice(optionIndex, 1);
+    setOptions([...(cloneOptions || [])]);
+    removeElement(cloneOptions);
+  };
+
+  const update = (value, label, optionIndex) => {
+    const cloneOptions = [...(options || [])];
+    cloneOptions[optionIndex][label] = value;
+    setOptions(cloneOptions);
+    updateElement(cloneOptions);
   };
 
   useEffect(() => {
@@ -76,26 +108,23 @@ export default function Table({ entry }) {
     <div className={classes.root}>
       <div style={{ display: "flex", alignItems: "center" }}>
         <label className={classes.label}>{addLabel}</label>
-        <button
+        <IconButton
+          size="small"
           className={classes.add}
           id={`cam-extensionElements-create-${id}`}
           onClick={addNewElement}
         >
-          <span>+</span>
-        </button>
+          <Add fontSize="small" />
+        </IconButton>
       </div>
-      {(showDefaultEntry || (options && options.length > 0)) && (
+      {options && options.length > 0 && (
         <table>
           <tbody>
-            <tr
-              style={{
-                display: "flex",
-                width: "100%",
-                justifyContent: "space-between",
-              }}
-            >
+            <tr className={classes.tableHead}>
               {labels.map((label) => (
-                <th key={label}>{label}</th>
+                <th key={label} className={classes.label}>
+                  {label}
+                </th>
               ))}
               <th key="add"></th>
             </tr>
@@ -107,36 +136,26 @@ export default function Table({ entry }) {
                       modelProperties.map((label, index) => (
                         <td key={`${option[label]}_${index}`}>
                           <input
+                            className={classes.input}
                             type="text"
                             defaultValue={option[label] || ""}
-                            style={{ width: "100%" }}
                             onBlur={(e) => {
-                              let isValid =
-                                validate &&
-                                validate(e.target.value, optionIndex);
-                              if (isValid) {
-                                updateElement(
-                                  e.target.value,
-                                  label,
-                                  option,
-                                  optionIndex
-                                );
-                              }
+                              update(e.target.value, label, optionIndex);
                             }}
                           />
                         </td>
                       ))}
                     <td>
-                      <button
+                      <IconButton
+                        size="small"
                         className={classes.clear}
                         id={`cam-extensionElements-remove-${id}`}
                         onClick={() => {
-                          removeElement(option, optionIndex);
-                          getOptions();
+                          remove(optionIndex);
                         }}
                       >
-                        <span style={{ fontSize: 10 }}>X</span>
-                      </button>
+                        <Close fontSize="small" />
+                      </IconButton>
                     </td>
                   </React.Fragment>
                 </tr>
