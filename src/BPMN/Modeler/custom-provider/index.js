37ebfb16a8610456250f1e6638f73f75b159b769
r.shah <r.shah@axelor.com>
2021-03-03 13:23:25 +0530

    Add comments support

--- src/BPMN/Modeler/custom-provider/index.js
+++ src/BPMN/Modeler/custom-provider/index.js
@@ -1,6 +1,8 @@
 import CustomPropertiesProvider from "./providers/CustomPropertiesProvider";
+import Comments from "./embedded-comments/comments";
 
 export default {
-  __init__: ["propertiesProvider"],
+  __init__: ["propertiesProvider", "comments"],
   propertiesProvider: ["type", CustomPropertiesProvider],
+  comments: ["type", Comments],
 };
