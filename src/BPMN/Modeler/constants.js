e1435fc0545af590f7b0b8639ce551b5031f749a
r.shah <r.shah@axelor.com>
2021-03-09 16:25:08 +0530

    Declare colors

---
+++ src/BPMN/Modeler/constants.js
@@ -0,0 +1,13 @@
+export const TASKCOLOR = {
+  "bpmn:Task": "#5EAEDA",
+  "bpmn:UserTask": "#3f97f6",
+  "bpmn:SendTask": "#F79000",
+  "bpmn:ReceiveTask": "#F8B200",
+  "bpmn:ManualTask": "#B567CA",
+  "bpmn:BusinessRuleTask": "#E76092",
+  "bpmn:ServiceTask": "#3EBFA5",
+  "bpmn:ScriptTask": "#3FC84C",
+  "bpmn:CallActivity": "#FBA729",
+  "bpmn:SubProcess": "#E4EBF8",
+  "bpmn:SequenceFlow": "#8095B3",
+};
