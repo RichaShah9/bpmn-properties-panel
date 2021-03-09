dc17e276724f8d66eb554881993dfc960d739342
r.shah <r.shah@axelor.com>
2021-03-09 16:25:21 +0530

    Remove custom renderer

--- src/BPMN/Modeler/custom-provider/index.js
+++ src/BPMN/Modeler/custom-provider/index.js
@@ -1,10 +1,8 @@
 import CustomPropertiesProvider from "./providers/CustomPropertiesProvider";
 import Comments from "./embedded-comments/comments";
-import CustomRenderer from "./providers/CustomRenderer";
 
 export default {
-  __init__: ["propertiesProvider", "comments", "customRenderer"],
+  __init__: ["propertiesProvider", "comments"],
   propertiesProvider: ["type", CustomPropertiesProvider],
   comments: ["type", Comments],
-  customRenderer: ["type", CustomRenderer],
 };
