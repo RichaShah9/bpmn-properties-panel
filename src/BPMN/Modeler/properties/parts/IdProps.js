11c38cdabac89b3ab0a6b009e29e38a70408744b
r.shah <r.shah@axelor.com>
2021-04-07 17:08:25 +0530

    RM 36915Allow to change id on element

--- src/BPMN/Modeler/properties/parts/IdProps.js
+++ src/BPMN/Modeler/properties/parts/IdProps.js
@@ -15,8 +15,8 @@ export default function IdProps(group, element, translate, options) {
     },
     setProperty: function (element, properties) {
       element.id = properties["id"];
-      if (element.businessObject && element.businessObject.id) {
-        element.businessObject.id = properties["id"];
+      if (element.businessObject) {
+        element.businessObject["id"] = properties["id"];
       }
     },
     validate: function (element, values) {
