121cf36c31380aa2b37bfba3531c39ea5276b0e3
r.shah <r.shah@axelor.com>
2021-04-28 14:35:00 +0530

    RM 37487 Remove collapse pool

--- src/BPMN/Modeler/custom-provider/index.js
+++ src/BPMN/Modeler/custom-provider/index.js
@@ -1,6 +1,8 @@
 import CustomPropertiesProvider from "./providers/CustomPropertiesProvider";
+import CustomReplaceMenuProvider from "./providers/ReplaceMenuProvider";
 
 export default {
-  __init__: ["propertiesProvider"],
+  __init__: ["propertiesProvider", "replaceMenuProvider"],
   propertiesProvider: ["type", CustomPropertiesProvider],
+  replaceMenuProvider: ["type", CustomReplaceMenuProvider],
 };
