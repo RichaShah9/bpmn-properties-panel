b7b6e3446f25163aa213da8f5d42acf71ed5fb79
r.shah <r.shah@axelor.com>
2021-05-25 19:00:40 +0530

    Update DMN version

--- src/DMN/DMNModeler.js
+++ src/DMN/DMNModeler.js
@@ -13,6 +13,13 @@ import Tooltip from "../components/Tooltip";
 
 import { download } from "../utils";
 import "dmn-js-properties-panel/dist/assets/dmn-js-properties-panel.css";
+import "dmn-js/dist/assets/dmn-js-decision-table-controls.css";
+import "dmn-js/dist/assets/dmn-js-decision-table.css";
+import "dmn-js/dist/assets/dmn-js-drd.css";
+import "dmn-js/dist/assets/dmn-js-literal-expression.css";
+import "dmn-js/dist/assets/dmn-js-shared.css";
+import "dmn-js/dist/assets/diagram-js.css";
+
 import "./css/dmnModeler.css";
 
 let dmnModeler = null;
